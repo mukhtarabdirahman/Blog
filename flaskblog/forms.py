352c1338fbb36b1989725b26031437cafe0d126d
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2020-01-20 01:37:52 +0300

    update accout

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -19,6 +19,7 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('Username already taken. Please create a new username.')
+        
     def validate_email(self, email):
     
         user = User.query.filter_by(email=email.data).first()
@@ -30,3 +31,12 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update Profile')
+
+
