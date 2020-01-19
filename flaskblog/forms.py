c19714e67505df27da9336afda34b07d42cfdae3
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2020-01-20 01:23:36 +0300

    login form

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -24,3 +24,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('email already taken. Please create a new email.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
